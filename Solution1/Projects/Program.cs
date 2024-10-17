13c4e9a6df72931681baea4848a3ebdf653d138f
A7medEsam13 <aesam4168@gmail.com>
2024-10-17 21:38:36 +0300

    Update Program.cs

--- Solution1/Projects/Program.cs
+++ Solution1/Projects/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 
-using Projects.Delegate;
 namespace Projects
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            App.Run();
+            
         }
     }
 }
